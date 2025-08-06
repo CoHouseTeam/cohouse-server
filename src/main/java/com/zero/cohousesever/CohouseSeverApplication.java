1e807a54a4df815524c11ea4c0641f94c80fc953
JYP <bluebinarycode@gmail.com>
2025-08-06 22:08:14 +0900

    first commit

---
+++ src/main/java/com/zero/cohousesever/CohouseSeverApplication.java
@@ -0,0 +1,13 @@
+package com.zero.cohousesever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohouseSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CohouseSeverApplication.class, args);
+    }
+
+}
