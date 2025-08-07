3c003ad763b324e915933d81b7ccd61dfba77010
escha-k <bbs7380@gmail.com>
2025-08-07 18:57:00 +0900

    feat: 멤버 관련 컨트롤러, DTO 셋업
    인증(로그인) 관련 컨트롤러, DTO 추가
    프로필 관련 컨트롤러, DTO 추가

---
+++ src/main/java/com/zero/cohousesever/member/dto/MessageDto.java
@@ -0,0 +1,12 @@
+package com.zero.cohousesever.member.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageDto {
+
+    private String message;
+}
