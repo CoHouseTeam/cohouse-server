d30c683f6d8a6549a4a39085505b19c0b59517a4
escha-k <bbs7380@gmail.com>
2025-08-11 15:24:40 +0900

    feat: 그룹 탈퇴 요청 엔티티, 리포지토리 생성

---
+++ src/main/java/com/zero/cohousesever/group/entity/GroupLeaveRequest.java
@@ -0,0 +1,32 @@
+package com.zero.cohousesever.group.entity;
+
+import com.zero.cohousesever.group.enums.LeaveRequestStatus;
+import com.zero.cohousesever.member.entity.Member;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class GroupLeaveRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    private String reason;
+
+    @Enumerated(EnumType.STRING)
+    private LeaveRequestStatus status;
+
+    private LocalDateTime requestedAt;
+
+    private LocalDateTime respondedAt;
+}
