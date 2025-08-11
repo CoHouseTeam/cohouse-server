166ae2844454024ea43cd2e59cd00e004c442dd4
JYP <bluebinarycode@gmail.com>
2025-08-11 21:34:05 +0900

    feat: BaseEntity 생성 및 Auditing 기능 테스트 추가

---
+++ src/main/java/com/zero/cohousesever/user/User.java
@@ -0,0 +1,19 @@
+package com.zero.cohousesever.user;
+
+import com.zero.cohousesever.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+
+    private String email;
+}
