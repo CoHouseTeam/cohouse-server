041d2d8bcac3e5c95e8bc92d60edf1096df1f0bd
escha-k <bbs7380@gmail.com>
2025-08-11 18:36:02 +0900

    feat: 그룹 컨트롤러, DTO 생성

--- src/main/java/com/zero/cohousesever/group/entity/Group.java
+++ src/main/java/com/zero/cohousesever/group/entity/Group.java
@@ -4,9 +4,11 @@ import com.zero.cohousesever.group.enums.GroupStatus;
 import jakarta.persistence.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.util.List;
+
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "`group`") // 예약어 이슈 방지
+@Table(name = "groups") // 예약어 이슈 방지
 public class Group {
 
     @Id
@@ -17,4 +19,7 @@ public class Group {
 
     @Enumerated(EnumType.STRING)
     private GroupStatus status;
+
+    @OneToMany
+    private List<GroupMember> members;
 }
