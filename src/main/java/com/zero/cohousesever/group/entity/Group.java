a637d4e59e7e2ab73093aa898c63a3873a230aba
escha-k <bbs7380@gmail.com>
2025-08-12 14:22:09 +0900

    refactor: 엔티티가 BaseEntity를 상속하도록 수정

--- src/main/java/com/zero/cohousesever/group/entity/Group.java
+++ src/main/java/com/zero/cohousesever/group/entity/Group.java
@@ -1,5 +1,6 @@
 package com.zero.cohousesever.group.entity;
 
+import com.zero.cohousesever.common.entity.BaseEntity;
 import com.zero.cohousesever.group.enums.GroupStatus;
 import jakarta.persistence.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,11 +10,7 @@ import java.util.List;
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "groups") // 예약어 이슈 방지
-public class Group {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Group extends BaseEntity {
 
     private String name;
 
