76ed849ebdcea099add70dd1f5c4a8252e9bcc52
escha-k <bbs7380@gmail.com>
2025-08-13 13:33:29 +0900

    feat: jwt 토큰 생성 클래스

---
+++ src/main/java/com/zero/cohousesever/member/security/JwtTokenProvider.java
@@ -0,0 +1,85 @@
+package com.zero.cohousesever.member.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 3600 * 1000; // 1시간
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 24 * 3600 * 1000; // 24시간
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_NAME = "name";
+
+
+    // TODO: 외부에서 이슈어, 비밀키 설정하기. 비밀키는 BASE64 인코딩된 문자열 사용
+    @Value("")
+    private String issuer;
+    @Value("")
+    private String secret;
+
+    public String generateAccessToken(String email, String name) {
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + ACCESS_TOKEN_EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .issuer(issuer)
+                .claims()
+                    .add(KEY_EMAIL, email)
+                    .add(KEY_NAME, name)
+                    .and()
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken() {
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + REFRESH_TOKEN_EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .issuer(issuer)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) {
+            return false;
+        }
+
+        Claims claims = parseClaims(token);
+        return !claims.getExpiration().before(new Date());
+    }
+
+    private Claims parseClaims(String token) {
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        JwtParser parser = Jwts.parser().verifyWith(secretKey).build();
+
+        try {
+            return parser.parseSignedClaims(token).getPayload();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    public String getEmailFromToken(String token) {
+        return parseClaims(token).get(KEY_EMAIL, String.class);
+    }
+}
