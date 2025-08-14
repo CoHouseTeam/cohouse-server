9c922b2c1cc27f0407236634f1b0fc0679acf366
escha-k <bbs7380@gmail.com>
2025-08-14 13:08:08 +0900

    fix: 어노테이션 오류 수정

--- src/main/java/com/zero/cohousesever/group/repository/GroupMemberRepository.java
+++ src/main/java/com/zero/cohousesever/group/repository/GroupMemberRepository.java
@@ -2,9 +2,9 @@ package com.zero.cohousesever.group.repository;
 
 import com.zero.cohousesever.group.entity.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
 
-@RestController
+@Repository
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
 
 }
