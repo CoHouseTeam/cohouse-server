145d1fc3ca3b68b6cffbe3999a9eaf3a3479e7a4
JYP <bluebinarycode@gmail.com>
2025-08-14 13:25:29 +0900

    refactor: DB와 Redis 연결 확인 로직의 출력 방식을 print에서 log로 변경

--- src/main/java/com/zero/cohousesever/common/config/check/RedisConnectionChecker.java
+++ src/main/java/com/zero/cohousesever/common/config/check/RedisConnectionChecker.java
@@ -1,11 +1,13 @@
 package com.zero.cohousesever.common.config.check;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RedisConnectionChecker implements ApplicationRunner {
@@ -15,14 +17,10 @@ public class RedisConnectionChecker implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
         try {
-            String pong = redisTemplate.getConnectionFactory().getConnection().ping();
-            if ("PONG".equalsIgnoreCase(pong)) { // 문자열 내용이 같으면 대소문자 구분 없이 true 반환
-                System.out.println("✅ Redis 연결 성공!");
-            } else {
-                System.out.println("⚠️  Redis 연결 실패");
-            }
+            redisTemplate.getConnectionFactory().getConnection().ping();
+            log.info("✅ Redis 연결 성공!");
         } catch (Exception e) {
-            System.out.println("❌ Redis 연결 오류: " + e.getMessage());
+            log.error("❌ Redis 연결 실패", e);
         }
     }
 }
