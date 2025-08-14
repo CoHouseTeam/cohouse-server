fa1cb995ba0007a972ce9401853ae4dd573efba6
escha-k <bbs7380@gmail.com>
2025-08-14 13:08:33 +0900

    feat: 어노테이션 정비/getter, builder 추가

--- src/main/java/com/zero/cohousesever/group/entity/GroupMember.java
+++ src/main/java/com/zero/cohousesever/group/entity/GroupMember.java
@@ -4,10 +4,18 @@ import com.zero.cohousesever.common.entity.BaseEntity;
 import com.zero.cohousesever.group.enums.GroupMemberStatus;
 import com.zero.cohousesever.member.entity.Member;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GroupMember extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
