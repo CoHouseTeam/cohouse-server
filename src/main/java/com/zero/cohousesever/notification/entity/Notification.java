f3f666c8729129e78c675d9b2981c46265a293ef
JYP <bluebinarycode@gmail.com>
2025-08-14 13:12:02 +0900

    refactor: Member 참조를 memberId에서 member로 변경

--- src/main/java/com/zero/cohousesever/notification/entity/Notification.java
+++ src/main/java/com/zero/cohousesever/notification/entity/Notification.java
@@ -2,10 +2,7 @@ package com.zero.cohousesever.notification.entity;
 
 import com.zero.cohousesever.common.entity.BaseEntity;
 import com.zero.cohousesever.member.entity.Member;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -17,9 +14,11 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Notification extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
-    private Member memberId;
+    private Member member;
 
-    private String type;
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
     private String title;
     private String content;
 
