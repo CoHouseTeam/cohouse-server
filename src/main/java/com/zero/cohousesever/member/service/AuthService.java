2e279521f6502a6230a2f7c302338aaa12c7318e
escha-k <bbs7380@gmail.com>
2025-08-15 19:04:05 +0900

    feat: 리프레시 토큰을 통한 액세스 토큰 재발급 기능 구현

--- src/main/java/com/zero/cohousesever/member/service/AuthService.java
+++ src/main/java/com/zero/cohousesever/member/service/AuthService.java
@@ -2,10 +2,13 @@ package com.zero.cohousesever.member.service;
 
 import com.zero.cohousesever.member.dto.auth.JwtTokenResponseDto;
 import com.zero.cohousesever.member.dto.auth.LoginRequestDto;
+import com.zero.cohousesever.member.dto.auth.RefreshRequestDto;
 import com.zero.cohousesever.member.dto.auth.SignupRequestDto;
 import com.zero.cohousesever.member.entity.Member;
 import com.zero.cohousesever.member.repository.MemberRepository;
+import com.zero.cohousesever.member.security.CustomUserDetails;
 import com.zero.cohousesever.member.security.JwtTokenProvider;
+import com.zero.cohousesever.member.security.TokenValidationStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,7 @@ import java.util.Objects;
 public class AuthService {
 
     private final MemberService memberService;
+    private final RefreshTokenService refreshTokenService;
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
@@ -57,7 +61,9 @@ public class AuthService {
         }
 
         String accessToken = jwtTokenProvider.generateAccessToken(member.getEmail(), member.getName());
-        String refreshToken = jwtTokenProvider.generateRefreshToken(); // TODO: Redis 서버에 리프레시토큰 저장
+        String refreshToken = jwtTokenProvider.generateRefreshToken();
+        // 리프레시 토큰은 redis에 저장
+        refreshTokenService.saveRefreshToken(member.getId(), refreshToken, JwtTokenProvider.REFRESH_TOKEN_EXPIRATION_TIME);
 
         return JwtTokenResponseDto.builder()
                 .accessToken(accessToken)
@@ -67,5 +73,29 @@ public class AuthService {
 
     public boolean isEmailDuplicated(String email) {
         return memberRepository.existsByEmail(email);
+    }
+
+    public JwtTokenResponseDto reissueAccessToken(CustomUserDetails userDetails, RefreshRequestDto requestDto) {
+        String memberRefreshToken = requestDto.getRefreshToken();
+        String serverRefreshToken = refreshTokenService.getRefreshToken(userDetails.getId());
+
+        if (!jwtTokenProvider.validateToken(memberRefreshToken).equals(TokenValidationStatus.VALID)) {
+            throw new RuntimeException(); // TODO: '유효하지 않은 리프레시 토큰입니다' 예외 작성
+        }
+
+        if (!jwtTokenProvider.validateToken(serverRefreshToken).equals(TokenValidationStatus.VALID)) {
+            throw new RuntimeException(); // TODO: '리프레시 토큰이 만료되었습니다.' 예외 작성
+        }
+
+        if (!Objects.equals(memberRefreshToken, serverRefreshToken)) {
+            throw new RuntimeException(); // TODO: '유효하지 않은 리프레시 토큰입니다' 예외 작성
+        }
+
+        String newAccessToken = jwtTokenProvider.generateAccessToken(userDetails.getEmail(), userDetails.getName());
+
+        return JwtTokenResponseDto.builder()
+                .accessToken(newAccessToken)
+                .refreshToken(serverRefreshToken)
+                .build();
     }
 }
