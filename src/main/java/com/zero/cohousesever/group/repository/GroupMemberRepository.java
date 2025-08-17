1fe35391fd1edbbedeb2d5bb458592daccd73d8b
escha-k <bbs7380@gmail.com>
2025-08-17 17:51:14 +0900

    feat: 멤버 아이디와 그룹 아이디로 그룹멤버를 조회하는 메서드 추가

--- src/main/java/com/zero/cohousesever/group/repository/GroupMemberRepository.java
+++ src/main/java/com/zero/cohousesever/group/repository/GroupMemberRepository.java
@@ -4,7 +4,11 @@ import com.zero.cohousesever.group.entity.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
+
+    Optional<GroupMember> findByMemberIdAndGroupId(Long memberId, Long groupId);
 
 }
