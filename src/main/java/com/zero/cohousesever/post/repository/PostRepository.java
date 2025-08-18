94ac1466f3b2ba9fc15120745c510d3947e3e7f5
HongmookChoi <leochoi224@gmail.com>
2025-08-18 19:57:51 +0900

    feat: post update

--- src/main/java/com/zero/cohousesever/post/repository/PostRepository.java
+++ src/main/java/com/zero/cohousesever/post/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.zero.cohousesever.post.repository;
 
 import com.zero.cohousesever.post.entity.Post;
+import com.zero.cohousesever.post.type.PostStatus;
 import com.zero.cohousesever.post.type.PostType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
@@ -17,4 +20,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
      * 그룹의 타입(공지/자유 등) 페이징 조회
      */
     Page<Post> findByGroupIdAndType(Long groupId, PostType type, Pageable pageable);
+
+    /**
+     * 상태값(게시중/삭제)으로 게시글 조회
+     */
+    Optional<Post> findByIdAndStatus(Long id,  PostStatus status);
 }
