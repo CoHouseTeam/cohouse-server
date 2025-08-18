f4bfb956b740a7b4d3eaa3779e277eda1f1ae6cd
escha-k <bbs7380@gmail.com>
2025-08-18 14:11:56 +0900

    feat: 메서드 추가

--- src/main/java/com/zero/cohousesever/group/repository/GroupMemberRepository.java
+++ src/main/java/com/zero/cohousesever/group/repository/GroupMemberRepository.java
@@ -5,10 +5,15 @@ import com.zero.cohousesever.group.enums.GroupMemberStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
 
     Optional<GroupMember> findByMemberIdAndStatus(Long memberId, GroupMemberStatus status);
+
+    List<GroupMember> findAllByGroupIdAndStatus(Long groupId, GroupMemberStatus status);
+
+    Boolean existsByMemberIdAndGroupIdAndStatus(Long memberId, Long groupId, GroupMemberStatus status);
 }
