c34df830147605df9a0c988a06fb57853e69b2de
escha-k <bbs7380@gmail.com>
2025-08-18 15:25:26 +0900

    feat: 특정 그룹 멤버를 조회하는 API 구현

--- src/main/java/com/zero/cohousesever/group/controller/GroupController.java
+++ src/main/java/com/zero/cohousesever/group/controller/GroupController.java
@@ -113,11 +113,15 @@ public class GroupController {
     // 멤버 상세 조회
     @GetMapping("/{groupId}/members/{groupMemberId}")
     public ResponseEntity<GroupMemberSummary> getGroupMember(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long groupId,
             @PathVariable("groupMemberId") Long groupMemberId
     ) {
+        Long memberId = userDetails.getId();
 
-        return ResponseEntity.ok().build();
+        GroupMemberSummary responseDto = groupService.getGroupMember(memberId, groupId, groupMemberId);
+
+        return ResponseEntity.ok(responseDto);
     }
 
     // 본인의 그룹 멤버 정보 수정
