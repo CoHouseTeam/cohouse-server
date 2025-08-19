602193505286f5607a6b0307940f35a1893d35ab
escha-k <bbs7380@gmail.com>
2025-08-19 15:29:36 +0900

    feat: CustomException 도입

--- src/main/java/com/zero/cohousesever/group/service/GroupService.java
+++ src/main/java/com/zero/cohousesever/group/service/GroupService.java
@@ -1,5 +1,6 @@
 package com.zero.cohousesever.group.service;
 
+import com.zero.cohousesever.common.exception.CustomException;
 import com.zero.cohousesever.group.dto.group.GroupNameDto;
 import com.zero.cohousesever.group.dto.group.GroupSummary;
 import com.zero.cohousesever.group.entity.Group;
@@ -16,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+import static com.zero.cohousesever.common.exception.ErrorCode.MEMBER_NOT_FOUND;
+
 @Service
 @RequiredArgsConstructor
 public class GroupService {
@@ -27,7 +30,8 @@ public class GroupService {
     @Transactional
     public GroupSummary createGroup(Long memberId, GroupNameDto groupNameDto) {
 
-        Member member = memberRepository.findById(memberId).orElseThrow(); // TODO: 적절한 예외 던지기
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
 
         GroupMember leader = GroupMember.builder()
                 .member(member)
