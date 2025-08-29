bb90e78ee4c9e79ca568bc13c631b30ca5b2c771
HongmookChoi <leochoi224@gmail.com>
2025-08-29 23:06:44 +0900

    feat: fcm-adapter

--- src/main/java/com/zero/cohousesever/CohouseSeverApplication.java
+++ src/main/java/com/zero/cohousesever/CohouseSeverApplication.java
@@ -3,7 +3,6 @@ package com.zero.cohousesever;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +11,5 @@ public class CohouseSeverApplication {
     public static void main(String[] args) {
         SpringApplication.run(CohouseSeverApplication.class, args);
     }
+
 }
