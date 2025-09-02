2d264795c11e4a558e092ec2abc61c1b1f2e5431
parkhongjoon <9030joon@naver.com>
2025-09-02 22:38:49 +0900

    Quartz

--- src/main/java/com/zero/cohousesever/common/config/QuartzConfig.java
+++ src/main/java/com/zero/cohousesever/common/config/QuartzConfig.java
@@ -19,6 +19,7 @@ public class QuartzConfig {
         .build();
   }
 
+  // 할 일 자동 배정
   @Bean
   public Trigger generateNextWeekTrigger(JobDetail generateNextWeekJobDetail) {
     CronScheduleBuilder cron = CronScheduleBuilder
